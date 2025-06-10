0761d79250c26ed0d855b1e2c25596fe23dc7098
VikVyaz <gluckundhappy@gmail.com>
2025-06-10 13:06:09 +0500

    admin, models, requirements, db settings

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'category', 'price', 'created_at', 'updated_at',)
+    list_filter = ('category', 'created_at', 'updated_at',)
+    search_fields = ('name', 'description',)
+
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,5 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    # path('home/', views.home, name='home'),  #<-- home.html поставлена за главную страницу
     path('contacts/', views.contacts, name='contacts'),
 ]
