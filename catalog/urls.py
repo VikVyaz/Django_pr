375e439ef1daf0423510609d241159ff4c25cdfa
VikVyaz <gluckundhappy@gmail.com>
2025-06-17 20:42:24 +0500

    Django Templating

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
+    path('products/<int:product_id>', views.product_details, name='product_details'),
+    path('to_add_product/', views.to_add_product, name='to_add_product'),
 ]
